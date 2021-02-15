504d0f295f4ba432c6997b4fac6f4ae510f106a6
Elidia Laronda <elaronda@ev-h6.kzn.21-school.ru>
2021-02-15 17:06:43 +0300

    .

--- tests.c
+++ tests.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   tests.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: elaronda <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/07 20:37:57 by elaronda          #+#    #+#             */
-/*   Updated: 2021/02/07 20:40:02 by elaronda         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libasm.h"
 
 void	ft_strlen_test(void)
@@ -110,10 +98,10 @@ void	ft_read_test(void)
 	write(1, BLUE, 5);
 	write(1, "\n\nOPEN | CREATE FILE", 20);
 	write(1, GREEN, 5);
-	read(open("tro", O_RDONLY), buf, 10);
+	read(open("tro", O_RDWR), buf, 10);
 	write(1, "\n>>> ", 5);
 	write(1, buf, 10);
-	ft_read(open("tro", O_RDONLY), buf, 10);
+	ft_read(open("tro", O_RDWR), buf, 10);
 	write(1, "<<< ", 4);
 	write(1, buf, 10);
 }
